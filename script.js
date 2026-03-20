4b42b3303e73e6d1a833144132f6f6c21f7777c6
lanrelegend-tech <lanrelegend@gmail.com>
2026-03-20 12:54:48 +0100

    ss

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let meals =[];
 async function getData() {
     try{
-    const response = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?=a');
+    const response = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=a');
     const data = await response.json();
 
     meals =data.meals || [];
@@ -15,20 +15,31 @@ const productContainer=document.getElementsByClassName('food-Container')[0];
 const displayMeals=()=> {
 productContainer.innerHTML='';
 let fragment = document.createDocumentFragment();
-if (! Array.isArray(meals)) {
-    console.log("meal is not array:",meals)
-meals.forEach(() =>{
+if ( Array.isArray(meals)) {
+meals.forEach(meal =>{
 const element=document.createElement('div');
 element.className='trend-card';
 
-element.innerHTML= '<h1>hello<h1>';
+element.innerHTML= `<img src ="${meal.strMealThumb}" alt="${meal.strMealThumb}">
+<h1 class="productname">${meal.strMeal}</h1>
+`;
+element.style.cursor ="pointer";
+element.addEventListener('click', () => {
+    if(meal.strSource) {
+        window.open(meal.strSource, '_blank');
+    } else{
+        alert(`${meal.strMeal}\n\n${meal.strInstructiona}`);
+    }
+});
 
 fragment.appendChild(element);
 
 });
 
 productContainer.appendChild(fragment);
-}    
+}    else{
+    console.log("meals is not an array:", meals);
+}
 
     };
     getData();
