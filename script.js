d6b0c4ee40c26db90f880a510e7f2a184cd61cd3
lanrelegend-tech <lanrelegend@gmail.com>
2026-03-20 12:12:28 +0100

    ii

--- script.js
+++ script.js
@@ -1,41 +1,32 @@
-const meals=[]
-
-
+let meals =[];
 async function getData() {
-    try{
-        const response=await fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=a');
-        const data=await response.json();
-        meals.push(data) 
-            console.log(meals); 
-    }catch(error){
-    console.error();
-    
-    }
+    const response = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?=');
+    const data = await response.json();
+
+    meals =data.meals;
     
     
 }
-const productContainer=document.getElementsByClassName('food-Container')
-const displaymeals=() =>{
-productContainer.innerHTML=''
-let fragment =document.createDocumentFragment();
-if(meals.length>0){
-meals.forEach(meal =>{
-let element=document.createElement('div')
-element.className='trend-card'
+function displayMeals() {
+const productContainer=document.getElementsByClassName('food-Container')[0];
+productContainer.innerHTML='';
+let fragment = document.createDocumentFragment();
+if(meals.length > 0) {
+meals.forEach(meals =>{
+const element=document.createElement('div');
+element.className='trend-card';
 
-elememt.innerHTML=`<img src="i1.jpg" alt="image">
-<h1 class="productname">product name</h1>`;
+element.innerHTML= `<img src="${meals.strMealThumb}" alt"${meals.strMeal}">
+                 <h1 class="Product name">${meals.strMeal}</h1>
+                 ` ;
 
-fragment.appendChild(element)
+fragment.appendChild(element);
 
 });
-
-productContainer.appendChild(fragment)
+}
+productContainer.appendChild(fragment);
         
 
-    }
-   }
-   
-   
+    };
+    displayMeals();
 
-displaymeals();
